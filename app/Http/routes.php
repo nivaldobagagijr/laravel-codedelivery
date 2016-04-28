9c11c4da3172b9e3773c93597854350176c18f57
Nivaldo Bagagi Jr <bagagi.junior@gmail.com>
2016-04-27 21:44:49 -0300

    CRUD Clientes atualizado

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,24 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('test', function() {
-	$repository = app()->make('CodeDelivery\Repositories\CategoryRepository');
-
-	return $repository->all();
+    return view('home');
 });
 
 Route::group(['prefix'=>'admin', 'middleware'=>'auth.checkrole', 'as'=>'admin.'], function(){
